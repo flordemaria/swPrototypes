337f7bc3692d01409ae797dde0ed61691c9806cd
Mauricio Castañeda <m.castanedam20@gmail.com>
2016-09-28 03:24:25 -0500

    Agregado historial notas pasadas, filtro de estado en casos, agregar alumno asignado, icono calendario a filtro, agrandar caja de texto nombres

--- js/clinica_form.js
+++ js/clinica_form.js
@@ -29,3 +29,14 @@ $(document).ready(function(){
 		document.getElementById('horario_info').innerHTML = horario;
 	});
 });
+$(document).ready(function(){
+    var date_input=$('input[name="date"]'); //our date input has the name "date"
+    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+    var options={
+        format: 'dd/mm/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+    };
+    date_input.datepicker(options);
+});
