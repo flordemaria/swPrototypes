c67a46f5f98346fbcd6284125399d58db6ddb8ea
mawcam <m.castanedam@pucp.pe>
2016-09-28 03:39:49 -0500

    date del cliente aún no funciona

--- js/clinica_form.js
+++ js/clinica_form.js
@@ -40,3 +40,14 @@ $(document).ready(function(){
     };
     date_input.datepicker(options);
 });
+$(document).ready(function(){
+    var date_input=$('input[name="date_cliente"]'); //our date input has the name "date"
+    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+    var options={
+        format: 'dd/mm/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+    };
+    date_input.datepicker(options);
+});
